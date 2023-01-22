ef0bed5034e49d992ca422018a435bce181a9b31
georgelofre <georgelofre@gmail.com>
2023-01-22 16:51:58 -0300

    questões 3 e 4

---
+++ 004/question_04.py
@@ -0,0 +1,9 @@
+def frase01(n1, n2):
+    print(f'{n1}, {n2}.')
+
+def frase02():
+    nome01 = input('Qual a primeira palavra: ')
+    nome02 = input('Qual a segunda palavra: ')
+    frase01(nome01, nome02)
+
+frase02()
