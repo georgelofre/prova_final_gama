f251fe39df821afe86c04064b0ff3ec553138fb2
georgelofre <georgelofre@gmail.com>
2023-01-22 20:31:33 -0300

    ajuste

--- 008/question_08.py
+++ 008/question_08.py
@@ -3,17 +3,17 @@ app = Flask(__name__)
 
 contador = 0
 
+@app.route('/contador', methods=['GET'])
+def get_contador():
+    global contador
+    return jsonify(numero=contador), 200
+
 @app.route('/contador', methods=['POST'])
 def post_contador():
     dado = request.get_json()
     global contador
     contador = dado["numero"]
     return jsonify(numero=contador), 201
-
-@app.route('/contador', methods=['GET'])
-def get_contador():
-    global contador
-    return jsonify(numero=contador), 200
 
 @app.route('/contador/incrementa', methods=['PUT'])
 def incrementa_contador():
