27edfb51fb95d9f181b7402e73d7f3dfba748a9b
georgelofre <georgelofre@gmail.com>
2023-01-22 16:34:09 -0300

    questões 1 e 2

---
+++ 003/question_03.py
@@ -0,0 +1,7 @@
+marcas = [{'id': 1, 'marca': 'corolla'}, {'id': 2, 'marca': 'honda'}]
+
+modelos = [{'id': 101, 'modelo': 'suv'}, {'id': 102, 'modelo': 'sedan'}, {'id': 103, 'modelo': 'hatch'}]
+
+carros = [{'id': 201, 'nome': 'corolla', 'marca_id': 1, 'modelo_id': 102},
+          {'id': 202, 'nome': 'crv', 'marca_id': 2, 'modelo_id': 101},
+          {'id': 203, 'nome': 'ettios', 'marca_id': 1, 'modelo_id': 103}]
